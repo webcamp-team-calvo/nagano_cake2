7e265c4d782153156df38758301c76d2f1d21f51
gmgn120@gmail.com <ec2-user@ip-172-31-73-181.ec2.internal>
2021-01-19 01:01:52 +0000

    Finish routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,30 @@
 Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
+  
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :categories, only: [:new, :create, :edit, :update]
+    resources :orders, only: [:index, :show, :update]
+    resources :order_items, only: [:update]
+    root to: 'homes#top'
+  end
+  
+  scope module: :public do
+    resource :customers, only: [:show, :edit, :update]
+    get '/customers/quit' => 'customers#quit'
+    patch '/customers/out' => 'customers#out'
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :update, :create, :destroy, :show]
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :orders, only: [:new, :create, :index, :show]
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/thanks' => 'orders#thanks'
+    resources :deliveries, only: [:index, :create, :destroy, :edit, :update]
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
