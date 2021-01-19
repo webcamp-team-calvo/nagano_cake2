8d4893312a05b715aa5c2840862f7d90a4b274a0
gmgn120 <gmgn120@gmail.com>
2021-01-19 06:58:16 +0000

    Add admin/categories/edit,update

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,4 +4,14 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
     @categories = Category.all
   end
+  
+  def edit
+    @category = Category.find(params[:id])
+  end
+  
+  def update
+    category = Category.find(params[:id])
+    category.update(category_params)
+    redirect_to new_admin_category_path
+  end
 end
