e0ecf1c9b58dc7ed413aa27b203247a927360abe
gmgn120@gmail.com <ec2-user@ip-172-31-73-181.ec2.internal>
2021-01-19 06:21:37 +0000

    Add admin/items/new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,18 @@
 class Admin::ItemsController < ApplicationController
+  
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :information, :image, :price)
+  end
 end
