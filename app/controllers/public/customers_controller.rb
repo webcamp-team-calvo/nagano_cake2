c7b0471f8c2809fc17eedd55dba791bd96fe1d59
sukepio <naoto927sukena@gmail.com>
2021-01-19 12:13:41 +0000

    Add public/customers/edit view

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,4 +3,8 @@ class Public::CustomersController < ApplicationController
   def show
     
   end
+  
+  def edit
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, skip: 'registrations'
   devise_for :admins
+  
+  devise_scope :customers do
+    get 'edit' => 'devise/registrations#edit'
+    get '/customers/sign_up' => 'devise/registrations#new', as: :new_customer_registration
+    post '/customers' => 'devise/registrations#create'
+  end
   
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
