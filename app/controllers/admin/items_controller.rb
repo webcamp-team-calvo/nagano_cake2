9b8dc2738b294c0078f99740a19adeff42212b44
gmgn120 <gmgn120@gmail.com>
2021-01-20 08:06:07 +0000

    Add admin/items/edit,update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -26,7 +26,7 @@ class Admin::ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     item.update(item_params)
-    redirect_to admin_item_path(item)
+    redirect_to admin_items_path
   end
 
   private
