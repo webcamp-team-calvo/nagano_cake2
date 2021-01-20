35a5af05010cfdd4f0eb1f7f87830dd4136a7a15
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-20 01:12:51 +0000

    successfully send itemdata to cart?

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,7 +2,9 @@ class Public::CartItemsController < ApplicationController
 
   def index
     #binding.pry
-
+    @item = Item.find(params[:item_id])
+    CartItem.create(customer_id: current_customer.id, item_id: @item.id, amount: params[:amount])
+    @cart_items = CartItem.all
   end
 
   def update
