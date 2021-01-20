f3420697b0d532391277681f8f87cb8d75e02fb8
sukepio <naoto927sukena@gmail.com>
2021-01-20 02:03:42 +0000

    disabled deleted customers from signing in

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -2,8 +2,7 @@
 
 class Customers::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  before_action :reject_isdeleted_customer, only: [:create]
-  
+before_action :reject_customer, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -18,20 +17,16 @@ class Customers::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
-  
-  def reject_isdeleted_customer
-    @customer = Customer.find_by(email: params[:customer][:email])
+
+  protected
+
+  def reject_customer
+    @customer = Customer.find_by(email: params[:customer][:email].downcase)
     if @customer
-      if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted
-        redirect_to new_customer_session_path
+      if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
+        redirect_to new_customer_registration_path
       end
     end
   end
-  
-  # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
