6d963ba876f146bcbc814e3cba7a58c5130eb01e
sukepio <naoto927sukena@gmail.com>
2021-01-20 10:02:29 +0000

    Add admin/customer show controller and view

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,4 +3,8 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.page(params[:page])
   end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
 end
