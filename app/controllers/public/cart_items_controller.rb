dfb31f344bcfc127bc7ba25ef1af71d7db3f35ad
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-20 06:44:29 +0000

    Update item/show form_with and Fix each do sentence of orderitems/index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,27 @@
 class Public::CartItemsController < ApplicationController
 
   def index
-    @item = Item.find(params[:item_id])
+    #@item = Item.find(params[:item_id])
     #@cart_item = CartItem.find_by(customer_id: current_customer.id, item_id: @item.id, amount: params[:amount])
-    CartItem.create(customer_id: current_customer.id, item_id: @item.id, amount: params[:amount])
+    #session[:cart_item] = @cart_item
+    #@cart_items = CartItem.all(session[:cart_item])
     @cart_items = CartItem.all
             #binding.pry
   end
 
   def update
+
+    @cart_item.update(cart_item_params)
+    @cart_items = CartItem.all
+    # redirect_to request.refererにするとindexでのcreateが実行されアイテムが増えてしまった。
+    render 'index'
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @cart_item = CartItem.find_by(customer_id: current_customer.id, item_id: @item.id, amount: params[:amount])
+    CartItem.create(customer_id: current_customer.id, item_id: @item.id, amount: params[:amount])
+    redirect_to cart_items_path
   end
 
   def destroy
@@ -23,6 +33,10 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
+  end
+
+  def cart_item_params
+    params.require(:cart_item).permit(:amount)
   end
 
 end
