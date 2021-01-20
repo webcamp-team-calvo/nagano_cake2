b13b9c9d34c553db8d6e9b5e11a112f22120ea51
sukepio <naoto927sukena@gmail.com>
2021-01-20 10:37:52 +0000

    Add admin/customer edit view

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,8 +3,16 @@ class Admin::CustomersController < ApplicationController
   def index
     @customers = Customer.page(params[:page])
   end
-  
+
   def show
     @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    
   end
 end
