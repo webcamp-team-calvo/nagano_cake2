eef4f5f4539f391aa9e45e78acadebd135a6ce55
sukepio <naoto927sukena@gmail.com>
2021-01-21 23:54:15 +0000

    fix bootstrap for admin/customer view

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,10 +1,15 @@
 class Delivery < ApplicationRecord
   belongs_to :customer
-  
+
   with_options presence: true do
 	  validates :postcode, format: {with: /\A\d{7}\z/}, numericality: true
 	  validates :name
 	  validates :address
 	end
-	
+
+
+	def view_all
+    '〒' + self.postcode + " " + self.address + "　" + self.name
+  end
+
 end
