62e4708c33d23fae53e2a8705895d03e3b3e7fde
gmgn120 <gmgn120@gmail.com>
2021-01-21 02:06:35 +0000

    Add public/orders/new

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,29 @@
 class Public::OrdersController < ApplicationController
+  
+  def new
+    @order = Order.new
+  end
+  
+  def confirm
+    
+  end
+  
+  def cretae
+  end
+  
+  def thanks
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:customer_id, :status, :postage, :total_payment, :payment_method, :postcode, :address, :name)
+  end
+  
 end
