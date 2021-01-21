c90bef32b6828d4ba623f97f2ae5fab0fadb9255
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-21 03:14:46 +0000

    Delete categories & admin/items controller and view

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,24 +1,2 @@
 class Admin::ItemsController < ApplicationController
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    # binding.pry
-    @item.save
-    redirect_to admin_items_path
-  end
-
-  def index
-    @items = Item.all
-  end
-
-  private
-
-  def item_params
-    params.require(:item).permit(:name, :information, :image, :price, :is_active, :category_id)
-  end
-
 end
