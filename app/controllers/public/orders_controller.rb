d9463629f5283f27d4304261ec07ae1b67472cfd
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-22 00:25:06 +0000

    Update orders/new view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,6 +9,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    #binding.pry
+    @order = Order.new(order_params)
+    #binding.pry
+    @order.save
+    redirect_to orders_confirm_path
   end
 
   def thanks
@@ -18,6 +23,10 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  def order_params
+    params.require(:order).permit(:customer_id, :payment_method, :postcode, :address, :name)
   end
 
 end
--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,10 +1,14 @@
 class Delivery < ApplicationRecord
   belongs_to :customer
-  
+
   with_options presence: true do
 	  validates :postcode, format: {with: /\A\d{7}\z/}, numericality: true
 	  validates :name
 	  validates :address
 	end
-	
+
+	def view_all
+    '〒' + self.postcode + " " + self.address + "　" + self.name
+  end
+
 end
