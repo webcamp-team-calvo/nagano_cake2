731717a4814209e0c8c2389529f5b1aa78b14a8b
zuttti25 <xxx.pxjpp@icloud.com>
2021-01-22 14:12:56 +0000

    First admin/orders/show

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,22 @@
 class Admin::OrderItemsController < ApplicationController
+  
+  def update
+    @order_item = OrderItem.find(params[:id])
+    @order = @order_item.order
+    @order_item.update(order_item_params)
+    if @order_item.making_status == "製作中"
+       @order.update(status: 2)
+    else
+      if @order.order_items.where(making_status: 3).count == @order.order_items.count
+         @order.update(status: 3)
+      end
+    end
+    redirect_to admins_order_item_path(@order)
+  end
+
+  protected
+  def order_item_params
+    params.require(:order_item).permit(:making_status)
+  end
+  
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,20 @@
 class Admin::OrdersController < ApplicationController
+  
+  def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items.includes(:item)
+    @total_price = @order_items.sum{|order_item|(order_item.price * 1.1).round * order_item.amount }
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    @order.update(order_params)
+    if @order.status == "入金待ち"
+       @order_items.update_all(making_status: 0)
+    elsif @order.status == "入金確認"
+      @order_items.update_all(making_status: 1)
+    end
+    redirect_to admins_order_item_path(@order)
+  end
 end
