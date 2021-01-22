41567e1dd365c10a8a339b3c9e91d24aca68ffb5
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-22 15:17:00 +0000

    Update selecttag from number to array

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,20 +11,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    #@item = Item.find(params[:item_id])
-    #CartItem.create(customer_id: current_customer.id, item_id: params[:item_id], amount: params[:amount])
     @cart_item = CartItem.new(customer_id: current_customer.id, item_id: params[:item_id], amount: params[:amount])
     @cart_items = CartItem.where(customer_id: current_customer.id)
     @cart_items.each do |cart_item|
       if cart_item.item_id == @cart_item.item_id
         total_amount = cart_item.amount + @cart_item.amount
         cart_item.update_attribute(:amount, total_amount)
-        #binding.pry
         @cart_item.delete
-       #@cart_item.create
       end
     end
-    #binding.pry
     @cart_item.save
 
     redirect_to cart_items_path
