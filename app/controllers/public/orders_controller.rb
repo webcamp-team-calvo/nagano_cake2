cce6f4946f4fc7124ae9a0ad236c8c888289f222
sukepio <naoto927sukena@gmail.com>
2021-01-23 00:47:33 +0000

    define enum for status

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -60,6 +60,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   private
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
+
+    enum status: {
+    "入金待ち": 0,
+    "入金確認": 1,
+    "製作中": 2,
+    "配送準備": 3,
+    "配送済み": 4
+  }
 end
