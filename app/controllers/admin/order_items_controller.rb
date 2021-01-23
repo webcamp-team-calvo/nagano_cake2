430376f7eece85319ed52f34fae617eca0cc3cce
sukepio <naoto927sukena@gmail.com>
2021-01-23 03:28:23 +0000

    borrow admin/order/show controller and view to check if automatic update of status works

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,22 @@
 class Admin::OrderItemsController < ApplicationController
+  
+  def update
+    @order_item = OrderItem.find(params[:id])
+    @order = @order_item.order
+    @order_item.update(order_item_params)
+    if @order_item.making_status == "製作中"
+       @order.update(status: 2)
+    elsif
+      @order.order_items.where(making_status: 3).count == @order.order_items.count
+      @order.update(status: 3)
+    end
+    
+    redirect_to admin_order_path(@order)
+  end
+
+  protected
+  def order_item_params
+    params.require(:order_item).permit(:making_status)
+  end
+  
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,25 @@
 class Admin::OrdersController < ApplicationController
+
+  def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    @order.update(order_params)
+    if @order.status == "入金待ち"
+       @order_items.update_all(making_status: 0)
+    elsif @order.status == "入金確認"
+      @order_items.update_all(making_status: 1)
+    end
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
