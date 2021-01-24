854a7b4ab00ea7cea6f9019689ecb2839604e56b
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-01-24 00:02:57 +0000

    Update public/items show select tag

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,15 @@
 class Public::ItemsController < ApplicationController
-  
+
   def index
     @active_items = Item.where(is_active: true).page(params[:page])
   end
 
   def show
     @item = Item.find(params[:id])
+
+    @amounts_array = ["個数選択", "1", "2", "3"]
+    #@first_choice_array = "個数選択".to_i
+
   end
 
 end
