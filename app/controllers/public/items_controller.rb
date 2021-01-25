0d6cf093c26106bbf93ba02356818d18e7bce8a9
zuttti25 <xxx.pxjpp@icloud.com>
2021-01-25 05:31:26 +0000

    Finish errormessage

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,9 +5,11 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
-    @amounts_array = ["個数選択", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    #@item = Item.find(params[:id])
+    #@amounts_array = ["個数選択", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     #@first_choice_array = "個数選択".to_i
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
 
   end
 
